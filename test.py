eeec8b2f85ee36d1d7dbd3b89a5b97ac43bf410c
nolanwinsman <33609548+nolanwinsman@users.noreply.github.com>
2021-03-01 16:37:29 -0600

    Added an argparse to make changing scenarios easier
    Not working perfectly but gets the job done quite well

--- test.py
+++ test.py
@@ -4,10 +4,62 @@ from vizdoom import *
 import random
 import time
 import itertools as it
+import argparse
+
+#Funtion that returns the proper name of the scenarios in the scenarios directory
+#Should probably use cases
+def findScenario(s, p):
+    ext = '.cfg'
+    if (s == 'basic'):
+        return p+'basic'+ext
+    elif (s == 'cig'): 
+        return p+'cig'+ext
+    elif (s == 'deadly' or 'deadly_corridor'):
+        return p+'deadly_corridor'+ext
+    elif (s == 'deathmatch'):
+        return p+'deathmatch'+ext
+    elif (s == 'defend_center' or s == 'defend_the_center'):
+        return p+'defend_the_center'+ext
+    elif (s == 'defend_line' or s == 'defend_the_line'):
+        return p+'defend_the_line'+ext
+    elif (s == 'health_gathering'):
+        return p+'health_gathering'+ext
+    elif (s == 'health_gathering_supreme'):
+        return p+'health_gathering_supreme'+ext
+    elif (s == 'learning'):
+        return p+'learning'+ext
+    elif (s == 'multi'):
+        return p+'multi'+ext
+    elif (s == 'multi_duel'):
+        return p+'multi_duel'+ext
+    elif (s == 'my_way_home' or s == 'home'):
+        return p+'my_way_home'+ext
+    elif (s == 'oblige'):
+        return p+'oblige'+ext
+    elif (s == 'predict_position'):
+        return p+'predict_position'+ext
+    elif (s == 'rocket_basic'):
+        return p+'rocket_basic'+ext
+    elif (s == 'simpler_basic'):
+        return p+'simpler_basic'+ext
+    elif (s == 'take_cover'):
+        return p+'take_cover'+ext
+    elif (s == 'test'):
+        return p+'test'+ext
+    else:
+        print('Scenario '+s+' Not Found, Loading basic.cfg')
+        return p+'basic'+ext
+
 
 game = DoomGame()
+
+parser = argparse.ArgumentParser(description = "Something")
+parser.add_argument('-scenario','-s', type=str, default = 'basic', help='Doom Scenario')
+args = parser.parse_args()
+scenario = findScenario(args.scenario, r"scenarios/")
+
 #game.load_config("/vizdoom/scenarios/test.cfg")
-game.load_config("scenarios/deadly_corridor.cfg")
+game.load_config(scenario)
 game.init()
 
 actions_num = game.get_available_buttons_size()
