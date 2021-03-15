3f67ce045cde4fff247636813ecc3dceb4d27b42
nolanwinsman <68390327+nolanwinsman@users.noreply.github.com>
2021-03-15 17:17:44 -0500

    Created a python file to plot all our data

---
+++ graph-data.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+
+
+
+
+def plotData(listX, listY):
+    plt.plot(listX, listY)
+    plt.show()
+def loadInData():
+    print("This function will laod in the data in each result.txt file and make a list of x coordinates and y coordinates")
+    x = []
+    y = []
+    return x,y
+
+
+if __name__ == '__main__':
+    x = [0, 1, 2]
+    y = [4, 8, 2]
+    plotData(x,y)
