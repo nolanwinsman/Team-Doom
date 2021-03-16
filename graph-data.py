84abd1dc6ab051d3aa09fa7cf1155745eb5eb06f
nolanwinsman <68390327+nolanwinsman@users.noreply.github.com>
2021-03-15 20:59:30 -0500

    Added labels to the graph

--- graph-data.py
+++ graph-data.py
@@ -5,6 +5,9 @@ from matplotlib import pyplot as plt
 
 def plotData(listX, listY):
     plt.plot(listX, listY)
+    plt.title("Reward Per Episode")
+    plt.xlabel("Episodes")
+    plt.ylabel("Reward")
     plt.show()
 def loadInData():
     print("This function will laod in the data in each result.txt file and make a list of x coordinates and y coordinates")
